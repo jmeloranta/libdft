3812eb6cbf810b7710ab73a189db4ee6db3f12c8
Jussi Eloranta <Jussi.Eloranta@gmail.com>
2015-10-06 12:01:27 -0700

    code sync

--- examples/3d/added_mass_viscosity/added_mass3.c
+++ examples/3d/added_mass_viscosity/added_mass3.c
@@ -22,10 +22,10 @@
 #define OUTPUT     500	/* output every this iteration */
 #define THREADS 0	/* # of parallel threads to use (0 = all) */
 #define PLANNING 1     /* 0 = estimate, 1 = measure, 2 = patient, 3 = exhaustive */
-#define NX 512       	/* # of grid points along x */
+#define NX 1024       	/* # of grid points along x */
 #define NY 512         /* # of grid points along y */
 #define NZ 512      	/* # of grid points along z */
-#define STEP 1.0        /* spatial step length (Bohr) */
+#define STEP 0.8        /* spatial step length (Bohr) */
 
 #define HELIUM_MASS (4.002602 / GRID_AUTOAMU) /* helium mass */
 #define IMP_MASS 1.0 /* electron mass */
@@ -33,7 +33,7 @@
 #define PSPOT "/home2/git/libdft/examples/3d/electron/jortner.dat"
 
 /* velocity components */
-#define KX	(1.0 * 2.0 * M_PI / (NX * STEP))
+#define KX	(2.0 * 2.0 * M_PI / (NX * STEP))
 #define KY	(0.0 * 2.0 * M_PI / (NY * STEP))
 #define KZ	(0.0 * 2.0 * M_PI / (NZ * STEP))
 #define VX	(KX * HBAR / HELIUM_MASS)
@@ -41,8 +41,8 @@
 #define VZ	(KZ * HBAR / HELIUM_MASS)
 #define EKIN	(0.5 * HELIUM_MASS * (VX * VX + VY * VY + VZ * VZ))
 
-#define T1200MK
-#define EPSILON 5E-5
+#define T2100MK
+#define EPSILON 5E-7
 
 #if 0
 /* debug */
