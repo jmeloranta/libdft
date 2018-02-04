c7f1cce22ac140d8f0e86dba8f958d3dbeda828e
Jussi Eloranta <jmeloranta@gmail.com>
2018-02-04 13:33:54 -0800

    vring

--- examples/3d/vortex-ring/vring.c
+++ examples/3d/vortex-ring/vring.c
@@ -13,17 +13,17 @@
 #include <dft/dft.h>
 #include <dft/ot.h>
 
-#define TS 20.0 /* fs */
+#define TS 50.0 /* fs */
 #define NX 128
 #define NY 128
 #define NZ 128
-#define STEP 2.0
-#define NTH 10
+#define STEP 0.5
+#define NTH 1000
 #define THREADS 4
 
-#define RING_RADIUS 30.0
+#define RING_RADIUS 5.3
 
-#define PRESSURE (1.0 / GRID_AUTOBAR)
+#define PRESSURE (0.0 / GRID_AUTOBAR)
 
 #define HELIUM_MASS (4.002602 / GRID_AUTOAMU)
 
@@ -36,22 +36,23 @@ double complex vring(void *asd, double x, double y, double z) {
   double ys = z;
   double angle = atan2(ys,xs), r = sqrt(xs*xs + ys*ys);
 
+//  return sqrt(rho0);
   return (1.0 - exp(-r * r / 2.0)) * sqrt(rho0) * cexp(I * angle);
 }
 
 int main(int argc, char **argv) {
 
-  rgrid3d *ext_pot, *rworkspace;
+  rgrid3d *ext_pot;
   cgrid3d *potential_store;
   wf3d *gwf, *gwfp;
   long iter;
-  double energy, mu0;
+  double mu0, kin, pot, n;
   char buf[512];
 
   /* Setup DFT driver parameters (grid) */
   dft_driver_setup_grid(NX, NY, NZ, STEP, THREADS);
   /* Plain Orsay-Trento in imaginary time */
-  dft_driver_setup_model(DFT_OT_PLAIN, DFT_DRIVER_IMAG_TIME, 0.0);
+  dft_driver_setup_model(DFT_OT_PLAIN | DFT_OT_KC | DFT_OT_BACKFLOW, DFT_DRIVER_IMAG_TIME, 0.0);
   /* No absorbing boundary */
   dft_driver_setup_boundary_type(DFT_DRIVER_BOUNDARY_REGULAR, 0.0, 0.0);
   /* Normalization condition */
@@ -65,10 +66,10 @@ int main(int argc, char **argv) {
   dft_driver_otf->rho0 = rho0;
   /* chemical potential */
   mu0 = dft_ot_bulk_chempot_pressurized(dft_driver_otf, PRESSURE);
+  printf("rho0 = %le Angs^-3, mu0 = %le K.\n", rho0 / (GRID_AUTOANG * GRID_AUTOANG * GRID_AUTOANG), mu0 * GRID_AUTOK);
 
   /* Allocate space for external potential */
   ext_pot = dft_driver_alloc_rgrid();
-  rworkspace = dft_driver_alloc_rgrid();
   potential_store = dft_driver_alloc_cgrid(); /* temporary storage */
 
   /* Allocate space for wavefunctions (initialized to sqrt(rho0)) */
@@ -84,8 +85,17 @@ int main(int argc, char **argv) {
     if(iter == 1 || !(iter % NTH)) {
       sprintf(buf, "vring-%ld", iter);
       dft_driver_write_grid(gwf->grid, buf);
+      dft_driver_propagate_predict(DFT_DRIVER_PROPAGATE_HELIUM, ext_pot, gwf, gwfp, potential_store, TS, iter);
+      dft_driver_propagate_correct(DFT_DRIVER_PROPAGATE_HELIUM, ext_pot, gwf, gwfp, potential_store, TS, iter);
+      kin = dft_driver_kinetic_energy(gwf);            /* Kinetic energy for gwf */
+      pot = dft_driver_potential_energy(gwf, ext_pot); /* Potential energy for gwf */
+      n = dft_driver_natoms(gwf);
+      printf("Iteration %ld helium natoms    = %le particles.\n", iter, n);   /* Energy / particle in K */
+      printf("Iteration %ld helium kinetic   = %.30lf\n", iter, kin * GRID_AUTOK);  /* Print result in K */
+      printf("Iteration %ld helium potential = %.30lf\n", iter, pot * GRID_AUTOK);  /* Print result in K */
+      printf("Iteration %ld helium energy    = %.30lf\n", iter, (kin + pot) * GRID_AUTOK);  /* Print result in K */
+      fflush(stdout);
     }
-    dft_driver_propagate_predict(DFT_DRIVER_PROPAGATE_HELIUM, ext_pot, gwf, gwfp, potential_store, TS, iter);
-    dft_driver_propagate_correct(DFT_DRIVER_PROPAGATE_HELIUM, ext_pot, gwf, gwfp, potential_store, TS, iter);
   }
+  return 0;
 }
