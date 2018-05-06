732bb865d224a3558918fa7b2d6b93d565b4f359
jme <jmeloranta@gmail.com>
2018-05-06 14:49:50 -0700

    Print vector components of the drag foces in bubble.c

--- examples/gas-bubble/analyze.c
+++ examples/gas-bubble/analyze.c
@@ -7,9 +7,12 @@
 
 void analyze(wf *wf, INT iter, REAL vx) {
 
-  static REAL cur_mom = 0.0, prev_mom = 0.0;
+  static REAL cur_mom_x = 0.0, cur_mom_y = 0.0, cur_mom_z = 0.0;
+  static REAL prev_mom_x = 0.0, prev_mom_y = 0.0, prev_mom_z = 0.0;
   rgrid *cur_x, *cur_y, *cur_z, *circ;
-  extern REAL dpot_func(void *, REAL, REAL, REAL);
+  extern REAL dpot_func_x(void *, REAL, REAL, REAL);
+  extern REAL dpot_func_y(void *, REAL, REAL, REAL);
+  extern REAL dpot_func_z(void *, REAL, REAL, REAL);
 
   printf("Current time = " FMT_R " fs.\n", ((REAL) iter) * TIME_STEP * GRID_AUTOFS);
 
@@ -19,17 +22,25 @@ void analyze(wf *wf, INT iter, REAL vx) {
   circ = dft_driver_get_workspace(4, 1);
 
   grid_wf_probability_flux(wf, cur_x, cur_y, cur_z);
-  cur_mom = rgrid_integral(cur_x) * wf->mass;
+  cur_mom_x = rgrid_integral(cur_x) * wf->mass;
+  cur_mom_y = rgrid_integral(cur_y) * wf->mass;
+  cur_mom_z = rgrid_integral(cur_z) * wf->mass;
     
-  if(vx > 0.0) printf("Added mass = " FMT_R " He atoms.\n", cur_mom / (wf->mass * vx));
+  if(vx > 0.0) printf("Added mass = " FMT_R " He atoms.\n", cur_mom_x / (wf->mass * vx));
 
   grid_wf_density(wf, circ);
   printf("Number of He atoms = " FMT_R "\n", rgrid_integral(circ));
   if(iter > 0) {
-    printf("Drag force1 = " FMT_R " (au).\n", -rgrid_weighted_integral(circ, dpot_func, NULL)); // circ = density here
-    printf("Drag force2 = " FMT_R " (au).\n",  (cur_mom - prev_mom) / (TIME_STEP * ((REAL) OUTPUT_ITER) / GRID_AUTOFS));
+    printf("Drag force1_x = " FMT_R " (au).\n", -rgrid_weighted_integral(circ, dpot_func_x, NULL)); // circ = density here
+    printf("Drag force1_y = " FMT_R " (au).\n", -rgrid_weighted_integral(circ, dpot_func_y, NULL)); // circ = density here
+    printf("Drag force1_z = " FMT_R " (au).\n", -rgrid_weighted_integral(circ, dpot_func_z, NULL)); // circ = density here
+    printf("Drag force2_x = " FMT_R " (au).\n",  (cur_mom_x - prev_mom_x) / (TIME_STEP * ((REAL) OUTPUT_ITER) / GRID_AUTOFS));
+    printf("Drag force2_y = " FMT_R " (au).\n",  (cur_mom_y - prev_mom_y) / (TIME_STEP * ((REAL) OUTPUT_ITER) / GRID_AUTOFS));
+    printf("Drag force2_z = " FMT_R " (au).\n",  (cur_mom_z - prev_mom_z) / (TIME_STEP * ((REAL) OUTPUT_ITER) / GRID_AUTOFS));
   }
-  prev_mom = cur_mom;
+  prev_mom_x = cur_mom_x;
+  prev_mom_y = cur_mom_y;
+  prev_mom_z = cur_mom_z;
 
   rgrid_abs_rot(circ, cur_x, cur_y, cur_z);
   rgrid_power(circ, circ, NN);
--- examples/gas-bubble/pot.c
+++ examples/gas-bubble/pot.c
@@ -22,7 +22,7 @@ REAL pot_func(void *NA, REAL x, REAL y, REAL z) {
 }
 
 /* Impurity must always be at the origin (dU/dx) */
-REAL dpot_func(void *NA, REAL x, REAL y, REAL z) {
+REAL dpot_func_x(void *NA, REAL x, REAL y, REAL z) {
 
   REAL r, rp, r2, r3, r5, r7, r9, r11;
 
@@ -38,4 +38,42 @@ REAL dpot_func(void *NA, REAL x, REAL y, REAL z) {
   r11 = r9 * r2;
   
   return (x / rp) * (-A0 * A1 * EXP(-A1 * r) + 4.0 * A2 / r5 + 6.0 * A3 / r7 + 8.0 * A4 / r9 + 10.0 * A5 / r11);
+}
+
+/* Impurity must always be at the origin (dU/dy) */
+REAL dpot_func_y(void *NA, REAL x, REAL y, REAL z) {
+
+  REAL r, rp, r2, r3, r5, r7, r9, r11;
+
+  rp = SQRT(x * x + y * y + z * z);
+  r = rp - RADD;
+  if(r < RMIN) return 0.0;
+
+  r2 = r * r;
+  r3 = r2 * r;
+  r5 = r2 * r3;
+  r7 = r5 * r2;
+  r9 = r7 * r2;
+  r11 = r9 * r2;
+  
+  return (y / rp) * (-A0 * A1 * EXP(-A1 * r) + 4.0 * A2 / r5 + 6.0 * A3 / r7 + 8.0 * A4 / r9 + 10.0 * A5 / r11);
+}
+
+/* Impurity must always be at the origin (dU/dz) */
+REAL dpot_func_z(void *NA, REAL x, REAL y, REAL z) {
+
+  REAL r, rp, r2, r3, r5, r7, r9, r11;
+
+  rp = SQRT(x * x + y * y + z * z);
+  r = rp - RADD;
+  if(r < RMIN) return 0.0;
+
+  r2 = r * r;
+  r3 = r2 * r;
+  r5 = r2 * r3;
+  r7 = r5 * r2;
+  r9 = r7 * r2;
+  r11 = r9 * r2;
+  
+  return (z / rp) * (-A0 * A1 * EXP(-A1 * r) + 4.0 * A2 / r5 + 6.0 * A3 / r7 + 8.0 * A4 / r9 + 10.0 * A5 / r11);
 }
