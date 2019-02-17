4ed793f22f76487d34c603f310b3385ff5fa854e
Jussi Eloranta <eloranta@laptop.lan>
2019-02-16 19:37:46 -0800

    Allow evaluating KC and BF energies separately.

--- src/ot-energy.c
+++ src/ot-energy.c
@@ -21,8 +21,9 @@
  * Note: the single particle kinetic portion is NOT included.
  *       (use grid_wf_kinetic_energy() to calculate this separately)
  *
- * otf            = OT  functional structure (dft_ot_functional *; input).
- * energy_density = energy density grid (rgrid *; output).
+ * otf            = OT functional structure (dft_ot_functional *; input).
+ * energy_density = Energy density grid (rgrid *; output).
+ * wf             = Wafe function (wf *; input).
  *
  * Workspace usage (uses density as well):
  * GP: none
@@ -36,7 +37,7 @@
 
 EXPORT void dft_ot_energy_density(dft_ot_functional *otf, rgrid *energy_density, wf *wf) {
 
-  rgrid *workspace1, *workspace2, *workspace3, *workspace4, *workspace5, *workspace6, *workspace7, *workspace8;
+  rgrid *workspace1, *workspace2;
   rgrid *density;
 
   density = otf->density;  
@@ -55,16 +56,10 @@ EXPORT void dft_ot_energy_density(dft_ot_functional *otf, rgrid *energy_density,
     return;
   }
 
-  if(!otf->workspace1) otf->workspace1 = rgrid_clone(otf->density, "OTF workspace1");
-  if(!otf->workspace2) otf->workspace2 = rgrid_clone(otf->density, "OTF workspace2");
+  if(!otf->workspace1) otf->workspace1 = rgrid_clone(density, "OTF workspace 1");
+  if(!otf->workspace2) otf->workspace2 = rgrid_clone(density, "OTF workspace 2");
   workspace1 = otf->workspace1;
   workspace2 = otf->workspace2;
-  workspace3 = NULL;
-  workspace4 = NULL;
-  workspace5 = NULL;
-  workspace6 = NULL;
-  workspace7 = NULL;
-  workspace8 = NULL;
 
   /* transform rho (wrk1) */
   rgrid_copy(workspace1, density);
@@ -109,142 +104,176 @@ EXPORT void dft_ot_energy_density(dft_ot_functional *otf, rgrid *energy_density,
     rgrid_sum(energy_density, energy_density, workspace1);
   }
 
-  /* begin kinetic energy correlation energy density */
-  if(otf->model & DFT_OT_KC) { /* new code */
+  if(otf->model & DFT_OT_KC) dft_ot_energy_density_kc(otf, energy_density, wf, density);
 
-    if(!otf->workspace3) otf->workspace3 = rgrid_clone(otf->density, "OTF workspace3");
-    if(!otf->workspace4) otf->workspace4 = rgrid_clone(otf->density, "OTF workspace4");
-    if(!otf->workspace5) otf->workspace5 = rgrid_clone(otf->density, "OTF workspace5");
-    if(!otf->workspace6) otf->workspace6 = rgrid_clone(otf->density, "OTF workspace6");
-    if(!otf->workspace7) otf->workspace7 = rgrid_clone(otf->density, "OTF workspace7");
-    if(!otf->workspace8) otf->workspace8 = rgrid_clone(otf->density, "OTF workspace8");
-    workspace3 = otf->workspace3;
-    workspace4 = otf->workspace4;
-    workspace5 = otf->workspace5;
-    workspace6 = otf->workspace6;
-    workspace7 = otf->workspace7;
-    workspace8 = otf->workspace8;
+  if(otf->model & DFT_OT_BACKFLOW) dft_ot_energy_density_bf(otf, energy_density, wf, density);
+}
 
-    /* 1. convolute density with F to get \tilde{\rho} (wrk1) */
-    rgrid_copy(workspace2, density);
-    rgrid_fft(workspace2);
-    rgrid_fft_convolute(workspace1, workspace2, otf->gaussian_tf);   /* otf->gaussian_tf is already in Fourier space */    
-    rgrid_inverse_fft(workspace1);
+/*
+ * Evaluate KC energy density.
+ * 
+ * otf            = OT functional structure (dft_ot_functional *; input).
+ * energy_density = Energy density grid (rgrid *; output).
+ * wf             = Wave function (wf *; input).
+ * density        = Density (rgrid *; input). This can be otf->density.
+ *
+ */
 
-    /* 2. modify wrk1 from \tilde{\rho} to (1 - \tilde{\rho}/\rho_{0s} */
-    rgrid_multiply_and_add(workspace1, -1.0/otf->rho_0s, 1.0);
+EXPORT void dft_ot_energy_density_kc(dft_ot_functional *otf, rgrid *energy_density, wf *wf, rgrid *density) {
 
-    /* 3. gradient \rho to wrk3 (x), wrk4 (y), wrk5 (z) */
-    rgrid_fd_gradient_x(density, workspace3);
-    rgrid_fd_gradient_y(density, workspace4);
-    rgrid_fd_gradient_z(density, workspace5);
+  rgrid *workspace1, *workspace2, *workspace3, *workspace4, *workspace5, *workspace6, *workspace7, *workspace8;
+
+  if(!otf->workspace1) otf->workspace1 = rgrid_clone(density, "OTF workspace 1");
+  if(!otf->workspace2) otf->workspace2 = rgrid_clone(density, "OTF workspace 2");
+  if(!otf->workspace3) otf->workspace3 = rgrid_clone(density, "OTF workspace 3");
+  if(!otf->workspace4) otf->workspace4 = rgrid_clone(density, "OTF workspace 4");
+  if(!otf->workspace5) otf->workspace5 = rgrid_clone(density, "OTF workspace 5");
+  if(!otf->workspace6) otf->workspace6 = rgrid_clone(density, "OTF workspace 6");
+  if(!otf->workspace7) otf->workspace7 = rgrid_clone(density, "OTF workspace 7");
+  if(!otf->workspace8) otf->workspace8 = rgrid_clone(density, "OTF workspace 8");
+  workspace1 = otf->workspace1;
+  workspace2 = otf->workspace2;
+  workspace3 = otf->workspace3;
+  workspace4 = otf->workspace4;
+  workspace5 = otf->workspace5;
+  workspace6 = otf->workspace6;
+  workspace7 = otf->workspace7;
+  workspace8 = otf->workspace8;
+
+  /* 1. convolute density with F to get \tilde{\rho} (wrk1) */
+  rgrid_copy(workspace2, density);
+  rgrid_fft(workspace2);
+  rgrid_fft_convolute(workspace1, workspace2, otf->gaussian_tf);   /* otf->gaussian_tf is already in Fourier space */    
+  rgrid_inverse_fft(workspace1);
+
+  /* 2. modify wrk1 from \tilde{\rho} to (1 - \tilde{\rho}/\rho_{0s} */
+  rgrid_multiply_and_add(workspace1, -1.0/otf->rho_0s, 1.0);
+
+  /* 3. gradient \rho to wrk3 (x), wrk4 (y), wrk5 (z) */
+  rgrid_fd_gradient_x(density, workspace3);
+  rgrid_fd_gradient_y(density, workspace4);
+  rgrid_fd_gradient_z(density, workspace5);
     
-    /* 4. X component: wrk6 = wrk3 * wrk1 (wrk1 = (d/dx)\rho_x * (1 - \tilde{\rho}/\rho_{0s}) */
-    /*    Y component: wrk7 = wrk4 * wrk1 (wrk1 = (d/dy)\rho_y * (1 - \tilde{\rho}/\rho_{0s}) */
-    /*    Z component: wrk8 = wrk5 * wrk1 (wrk1 = (d/dz)\rho_z * (1 - \tilde{\rho}/\rho_{0s}) */
-    rgrid_product(workspace6, workspace3, workspace1);
-    rgrid_product(workspace7, workspace4, workspace1);
-    rgrid_product(workspace8, workspace5, workspace1);
+  /* 4. X component: wrk6 = wrk3 * wrk1 (wrk1 = (d/dx)\rho_x * (1 - \tilde{\rho}/\rho_{0s}) */
+  /*    Y component: wrk7 = wrk4 * wrk1 (wrk1 = (d/dy)\rho_y * (1 - \tilde{\rho}/\rho_{0s}) */
+  /*    Z component: wrk8 = wrk5 * wrk1 (wrk1 = (d/dz)\rho_z * (1 - \tilde{\rho}/\rho_{0s}) */
+  rgrid_product(workspace6, workspace3, workspace1);
+  rgrid_product(workspace7, workspace4, workspace1);
+  rgrid_product(workspace8, workspace5, workspace1);
 
-    /* 5. convolute (X): wrk6 = convolution(otf->gaussian * wrk6). */
-    /*    convolute (Y): wrk7 = convolution(otf->gaussian * wrk7). */
-    /*    convolute (Z): wrk8 = convolution(otf->gaussian * wrk8). */
-    rgrid_fft(workspace6);
-    rgrid_fft(workspace7);
-    rgrid_fft(workspace8);
-    rgrid_fft_convolute(workspace6, workspace6, otf->gaussian_tf);
-    rgrid_fft_convolute(workspace7, workspace7, otf->gaussian_tf);
-    rgrid_fft_convolute(workspace8, workspace8, otf->gaussian_tf);
-    rgrid_inverse_fft(workspace6);
-    rgrid_inverse_fft(workspace7);
-    rgrid_inverse_fft(workspace8);
+  /* 5. convolute (X): wrk6 = convolution(otf->gaussian * wrk6). */
+  /*    convolute (Y): wrk7 = convolution(otf->gaussian * wrk7). */
+  /*    convolute (Z): wrk8 = convolution(otf->gaussian * wrk8). */
+  rgrid_fft(workspace6);
+  rgrid_fft(workspace7);
+  rgrid_fft(workspace8);
+  rgrid_fft_convolute(workspace6, workspace6, otf->gaussian_tf);
+  rgrid_fft_convolute(workspace7, workspace7, otf->gaussian_tf);
+  rgrid_fft_convolute(workspace8, workspace8, otf->gaussian_tf);
+  rgrid_inverse_fft(workspace6);
+  rgrid_inverse_fft(workspace7);
+  rgrid_inverse_fft(workspace8);
 
-    /* 6. X: wrk6 = wrk6 * wrk3 * wrk1 */
-    /*    Y: wrk7 = wrk7 * wrk4 * wrk1 */
-    /*    Z: wrk8 = wrk8 * wrk5 * wrk1 */
-    rgrid_product(workspace6, workspace6, workspace3);
-    rgrid_product(workspace6, workspace6, workspace1);
-    rgrid_product(workspace7, workspace7, workspace4);
-    rgrid_product(workspace7, workspace7, workspace1);
-    rgrid_product(workspace8, workspace8, workspace5);
-    rgrid_product(workspace8, workspace8, workspace1);
+  /* 6. X: wrk6 = wrk6 * wrk3 * wrk1 */
+  /*    Y: wrk7 = wrk7 * wrk4 * wrk1 */
+  /*    Z: wrk8 = wrk8 * wrk5 * wrk1 */
+  rgrid_product(workspace6, workspace6, workspace3);
+  rgrid_product(workspace6, workspace6, workspace1);
+  rgrid_product(workspace7, workspace7, workspace4);
+  rgrid_product(workspace7, workspace7, workspace1);
+  rgrid_product(workspace8, workspace8, workspace5);
+  rgrid_product(workspace8, workspace8, workspace1);
     
-    /* 7. add wrk6 + wrk7 + wrk8 (components from the dot product) */
-    /* 8. multiply by -\hbar^2\alpha_s/(4M_{He}) */    
-    rgrid_add_scaled(energy_density, -otf->alpha_s / (4.0 * otf->mass), workspace6);
-    rgrid_add_scaled(energy_density, -otf->alpha_s / (4.0 * otf->mass), workspace7);
-    rgrid_add_scaled(energy_density, -otf->alpha_s / (4.0 * otf->mass), workspace8);
+  /* 7. add wrk6 + wrk7 + wrk8 (components from the dot product) */
+  /* 8. multiply by -\hbar^2\alpha_s/(4M_{He}) */    
+  rgrid_add_scaled(energy_density, -otf->alpha_s / (4.0 * otf->mass), workspace6);
+  rgrid_add_scaled(energy_density, -otf->alpha_s / (4.0 * otf->mass), workspace7);
+  rgrid_add_scaled(energy_density, -otf->alpha_s / (4.0 * otf->mass), workspace8);
+}
+
+/*
+ * Evaluate BF energy density.
+ * 
+ * otf            = OT functional structure (dft_ot_functional *; input).
+ * energy_density = Energy density grid (rgrid *; output).
+ * wf             = Wave function (wf *; input).
+ * density        = Density (rgrid *; input). This can be otf->density.
+ *
+ */
+
+EXPORT void dft_ot_energy_density_bf(dft_ot_functional *otf, rgrid *energy_density, wf *wf, rgrid *density) {
+
+  rgrid *workspace1, *workspace2, *workspace3, *workspace4, *workspace5, *workspace6, *workspace7;
+
+  if(!otf->workspace1) otf->workspace1 = rgrid_clone(density, "OTF workspace 1");
+  if(!otf->workspace2) otf->workspace2 = rgrid_clone(density, "OTF workspace 2");
+  if(!otf->workspace3) otf->workspace3 = rgrid_clone(density, "OTF workspace 3");
+  if(!otf->workspace4) otf->workspace4 = rgrid_clone(density, "OTF workspcae 4");
+  if(!otf->workspace5) otf->workspace5 = rgrid_clone(density, "OTF workspace 5");
+  if(!otf->workspace6) otf->workspace6 = rgrid_clone(density, "OTF workspace 6");
+  if(!otf->workspace7) otf->workspace7 = rgrid_clone(density, "OTF workspace 7");
+  workspace1 = otf->workspace1;
+  workspace2 = otf->workspace2;
+  workspace3 = otf->workspace3;
+  workspace4 = otf->workspace4;
+  workspace5 = otf->workspace5;
+  workspace6 = otf->workspace6;
+  workspace7 = otf->workspace7;
+
+  if((otf->model & DFT_OT_HD) || (otf->model & DFT_OT_HD2)) {
+    /* M & M high density cutoff */
+    grid_func2_operate_one(workspace7, density, otf->xi, otf->rhobf);
+  } else {
+    /* Original BF */
+    rgrid_copy(workspace7, density);
   }
-
-  if(otf->model & DFT_OT_BACKFLOW) {
-
-    if(!otf->workspace3) otf->workspace3 = rgrid_clone(otf->density, "OTF workspace 3");
-    if(!otf->workspace4) otf->workspace4 = rgrid_clone(otf->density, "OTF workspcae 4");
-    if(!otf->workspace5) otf->workspace5 = rgrid_clone(otf->density, "OTF workspace 5");
-    if(!otf->workspace6) otf->workspace6 = rgrid_clone(otf->density, "OTF workspace 6");
-    if(!otf->workspace7) otf->workspace7 = rgrid_clone(otf->density, "OTF workspace 7");
-    workspace3 = otf->workspace3;
-    workspace4 = otf->workspace4;
-    workspace5 = otf->workspace5;
-    workspace6 = otf->workspace6;
-    workspace7 = otf->workspace7;
-    workspace8 = NULL;
-
-    if((otf->model & DFT_OT_HD) || (otf->model & DFT_OT_HD2)) {
-      /* M & M high density cutoff */
-      grid_func2_operate_one(workspace7, density, otf->xi, otf->rhobf);
-    } else {
-      /* Original BF */
-      rgrid_copy(workspace7, density);
-    }
-    // workspace7 = density from this on
+  // workspace7 = density from this on
     
-    grid_wf_velocity(wf, workspace1, workspace2, workspace3, otf->veloc_cutoff);
-    rgrid_product(workspace4, workspace1, workspace1);   /* v_x^2 */
-    rgrid_product(workspace5, workspace2, workspace2);   /* v_y^2 */
-    rgrid_sum(workspace4, workspace4, workspace5);
-    rgrid_product(workspace5, workspace3, workspace3);   /* v_z^2 */
-    rgrid_sum(workspace4, workspace4, workspace5);       /* wrk4 = v_x^2 + v_y^2 + v_z^2 */
+  grid_wf_velocity(wf, workspace1, workspace2, workspace3, otf->veloc_cutoff);
+  rgrid_product(workspace4, workspace1, workspace1);   /* v_x^2 */
+  rgrid_product(workspace5, workspace2, workspace2);   /* v_y^2 */
+  rgrid_sum(workspace4, workspace4, workspace5);
+  rgrid_product(workspace5, workspace3, workspace3);   /* v_z^2 */
+  rgrid_sum(workspace4, workspace4, workspace5);       /* wrk4 = v_x^2 + v_y^2 + v_z^2 */
 
-    /* Term 1: -(M/4) * rho(r) * v(r)^2 \int U_j(|r - r'|) * rho(r') d3r' */
-    rgrid_copy(workspace5, workspace7);
-    rgrid_fft(workspace5);                        /* This was done before - TODO: save previous rho FFT and reuse here */
-    rgrid_fft_convolute(workspace6, otf->backflow_pot, workspace5);
-    rgrid_inverse_fft(workspace6);
-    rgrid_product(workspace6, workspace6, workspace4);
-    rgrid_product(workspace6, workspace6, workspace7);
-    rgrid_add_scaled(energy_density, -otf->mass / 4.0, workspace6);
-    /* Term 2: +(M/2) * rho(r) v(r) . \int U_j(|r - r'|) * rho(r') v(r') d3r' */
-    /* x contribution */
-    rgrid_product(workspace5, workspace7, workspace1);   /* rho(r') * v_x(r') */
-    rgrid_fft(workspace5);
-    rgrid_fft_convolute(workspace6, otf->backflow_pot, workspace5);
-    rgrid_inverse_fft(workspace6);
-    rgrid_product(workspace6, workspace6, workspace7);
-    rgrid_product(workspace6, workspace6, workspace1);
-    rgrid_add_scaled(energy_density, otf->mass / 2.0, workspace6);
-    /* y contribution */
-    rgrid_product(workspace5, workspace7, workspace2);   /* rho(r') * v_y(r') */
-    rgrid_fft(workspace5);
-    rgrid_fft_convolute(workspace6, otf->backflow_pot, workspace5);
-    rgrid_inverse_fft(workspace6);
-    rgrid_product(workspace6, workspace6, workspace7);
-    rgrid_product(workspace6, workspace6, workspace2);
-    rgrid_add_scaled(energy_density, otf->mass / 2.0, workspace6);
-    /* z contribution */
-    rgrid_product(workspace5, workspace7, workspace3);   /* rho(r') * v_z(r') */
-    rgrid_fft(workspace5);
-    rgrid_fft_convolute(workspace6, otf->backflow_pot, workspace5);
-    rgrid_inverse_fft(workspace6);
-    rgrid_product(workspace6, workspace6, workspace7);
-    rgrid_product(workspace6, workspace6, workspace3);
-    rgrid_add_scaled(energy_density, otf->mass / 2.0, workspace6);
-    /* Term 3: -(M/4) rho(r) \int U_j(|r - r'|) rho(r') v^2(r') d3r' */
-    rgrid_product(workspace5, workspace7, workspace4);
-    rgrid_fft(workspace5);
-    rgrid_fft_convolute(workspace6, otf->backflow_pot, workspace5);
-    rgrid_inverse_fft(workspace6);
-    rgrid_product(workspace6, workspace6, workspace7);
-    rgrid_add_scaled(energy_density, -otf->mass / 4.0, workspace6);
-  }
+  /* Term 1: -(M/4) * rho(r) * v(r)^2 \int U_j(|r - r'|) * rho(r') d3r' */
+  rgrid_copy(workspace5, workspace7);
+  rgrid_fft(workspace5);                        /* This was done before - TODO: save previous rho FFT and reuse here */
+  rgrid_fft_convolute(workspace6, otf->backflow_pot, workspace5);
+  rgrid_inverse_fft(workspace6);
+  rgrid_product(workspace6, workspace6, workspace4);
+  rgrid_product(workspace6, workspace6, workspace7);
+  rgrid_add_scaled(energy_density, -otf->mass / 4.0, workspace6);
+  /* Term 2: +(M/2) * rho(r) v(r) . \int U_j(|r - r'|) * rho(r') v(r') d3r' */
+  /* x contribution */
+  rgrid_product(workspace5, workspace7, workspace1);   /* rho(r') * v_x(r') */
+  rgrid_fft(workspace5);
+  rgrid_fft_convolute(workspace6, otf->backflow_pot, workspace5);
+  rgrid_inverse_fft(workspace6);
+  rgrid_product(workspace6, workspace6, workspace7);
+  rgrid_product(workspace6, workspace6, workspace1);
+  rgrid_add_scaled(energy_density, otf->mass / 2.0, workspace6);
+  /* y contribution */
+  rgrid_product(workspace5, workspace7, workspace2);   /* rho(r') * v_y(r') */
+  rgrid_fft(workspace5);
+  rgrid_fft_convolute(workspace6, otf->backflow_pot, workspace5);
+  rgrid_inverse_fft(workspace6);
+  rgrid_product(workspace6, workspace6, workspace7);
+  rgrid_product(workspace6, workspace6, workspace2);
+  rgrid_add_scaled(energy_density, otf->mass / 2.0, workspace6);
+  /* z contribution */
+  rgrid_product(workspace5, workspace7, workspace3);   /* rho(r') * v_z(r') */
+  rgrid_fft(workspace5);
+  rgrid_fft_convolute(workspace6, otf->backflow_pot, workspace5);
+  rgrid_inverse_fft(workspace6);
+  rgrid_product(workspace6, workspace6, workspace7);
+  rgrid_product(workspace6, workspace6, workspace3);
+  rgrid_add_scaled(energy_density, otf->mass / 2.0, workspace6);
+  /* Term 3: -(M/4) rho(r) \int U_j(|r - r'|) rho(r') v^2(r') d3r' */
+  rgrid_product(workspace5, workspace7, workspace4);
+  rgrid_fft(workspace5);
+  rgrid_fft_convolute(workspace6, otf->backflow_pot, workspace5);
+  rgrid_inverse_fft(workspace6);
+  rgrid_product(workspace6, workspace6, workspace7);
+  rgrid_add_scaled(energy_density, -otf->mass / 4.0, workspace6);
 }
